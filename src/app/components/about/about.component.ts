680a2de040c328a48512d447d0df18de8a783c7c
emeery <gerardoemery@live.com.mx>
2024-12-30 19:03:28 -0600

    test splitText dissapear

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,13 @@
-import { AfterViewInit, Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
+
+// import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { SplitText } from "gsap/SplitText";
 import { TextPlugin } from "gsap/TextPlugin";
 import p5 from 'p5'
-
+// declare var $:any;
 /* import { ModalComponent } from '../modal/modal.component'; */
 
 declare var VANTA: any;
@@ -17,19 +19,53 @@ declare var VANTA: any;
   styleUrl: './about.component.scss'
 })
 export class AboutComponent implements OnInit, AfterViewInit {
+  smoother: ScrollSmoother;
+  @ViewChild('wrapper') wrapperEl: ElementRef<HTMLElement>;
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
-    private modalService: NgbModal) {
-
-  }
+    // private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
-    gsap.registerPlugin(TextPlugin, ScrollTrigger, SplitText);
+    gsap.registerPlugin(TextPlugin, ScrollTrigger, SplitText, ScrollSmoother);
     let mm = gsap.matchMedia();
-    var split = new SplitText("#split", {type: "chars"});
-    let chars = split.chars;
+    var splitTitle = new SplitText("#split", {type: "chars"});
+    let chars = splitTitle.chars;
+
+    var mySplitText = new SplitText("#quote", { type: "words,chars" });
+    var arrayText = []
+    arrayText.push(mySplitText.chars)
+    arrayText.forEach((char, i) => {
+      const bg = "#F1F1F1";
+      const fg = "#0F0E1A";
+      const text = new SplitText(char, { types: "chars" });
+    
+      gsap.fromTo(
+        text.chars,
+        {
+          color: fg,
+        },
+        {
+          
+          color: bg,
+          duration: 1,
+          stagger: 1,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: char,
+            start: "top 60%",
+            end: "top 25%",
+            scrub: true,
+            markers: false,
+
+          }
+        }
+      );
+    });
+   
     // mobile
     mm.add("(max-width: 700px)", () => {
+      // gsap.set('#split', {perspective: 400})
       gsap.from(chars, {
         y: 130,
         stagger: 0.02,
@@ -74,6 +110,15 @@ export class AboutComponent implements OnInit, AfterViewInit {
     
   }
   ngAfterViewInit(): void {
+   /*  this.smoother = ScrollSmoother.create({
+      content: '#smooth-content',
+      wrapper: 'app-about',
+      smooth: 2,
+    }); */
+
+     
+
+
     VANTA.TRUNK({
       el: '#vanta-exp',
       mouseControls: true,
